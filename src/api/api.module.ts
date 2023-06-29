2a6ae0c838cd7c8607fb1c0720e16e865c320e99
spyle23 <sampingyen.lee@gmail.com>
2023-06-29 21:01:10 +0300

    add middleware

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { CommandModule } from './command/command.module';
 import { ProductModule } from './product/product.module';
 import { CategoryModule } from './category/category.module';
 import { AuthModule } from './auth/auth.module';
+import { LoggerMiddleware } from 'src/logger/logger.middleware';
 
 @Module({
   imports: [CommandModule, ProductModule, CategoryModule, AuthModule],
   controllers: [ApiController],
 })
-export class ApiModule {}
+export class ApiModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes();
+  }
+}
