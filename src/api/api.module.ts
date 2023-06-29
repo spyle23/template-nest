d3e55daf2897ce1dabbcca3c42f2c9b6743b5965
spyle23 <sampingyen.lee@gmail.com>
2023-06-29 22:07:45 +0300

    add upload features

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,4 +1,5 @@
 import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { MulterModule } from "@nestjs/platform-express/multer"
 import { ApiController } from './api.controller';
 import { CommandModule } from './command/command.module';
 import { ProductModule } from './product/product.module';
@@ -7,7 +8,7 @@ import { AuthModule } from './auth/auth.module';
 import { LoggerMiddleware } from 'src/logger/logger.middleware';
 
 @Module({
-  imports: [CommandModule, ProductModule, CategoryModule, AuthModule],
+  imports: [CommandModule, ProductModule, CategoryModule, AuthModule, MulterModule.register({ dest: "uploads/"  })],
   controllers: [ApiController],
 })
 export class ApiModule implements NestModule {
