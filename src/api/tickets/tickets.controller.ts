8fdc78d9481e3d6a3ed606135e36f5061aa897c6
spyle23 <sampingyen.lee@gmail.com>
2023-06-30 17:51:11 +0300

    create ticket

--- src/api/tickets/tickets.controller.ts
+++ src/api/tickets/tickets.controller.ts
@@ -1,19 +1,36 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Request as NestRequest } from '@nestjs/common';
 import { Prisma, Ticket, TicketType } from '@prisma/client';
 import { TicketsService } from './tickets.service';
 import { ResponseForm } from 'src/types/response';
+import { Request } from 'express';
+import { CustomRequest } from 'src/logger/logger.middleware';
+import { TicketArg } from 'src/types/ticket';
+
+const checkType = (startDate: Date,param: TicketType): Date => {
+  const result = new Date(startDate);
+  if(param === TicketType.UNIQUE || param === TicketType.MOIS){
+    result.setMonth(result.getMonth() + 1)
+  }else if(param === TicketType.JOUR) {
+    result.setDate(result.getDate() + 1)
+  }else {
+    result.setDate(result.getDate() + 7)
+  }
+  return result
+}
 
 @Controller('api/tickets')
 export class TicketsController {
   constructor(private ticketService: TicketsService) { }
 
   @Post()
-  async createTicket(@Body() body: Prisma.TicketCreateInput) {
-    const data = await this.ticketService.createTicket(body);
+  async createTicket(@Body() body: TicketArg & { busid?: number }, @NestRequest() req: CustomRequest) {
+    const endDate = checkType(new Date(), body.ticketType)
+    const nestedData = { ...body, useremail: req.user.email, endDate }
+    const data = await this.ticketService.createTicket(nestedData);
 
-    const response: ResponseForm<Ticket> = {
-      success: true,
-      message: 'ticket created',
+    const response: ResponseForm<Ticket | null> = {
+      success: data ? true : false,
+      message: data ? 'ticket created' : 'failed to create ticket',
       data,
     };
     return response;
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Global, Module } from '@nestjs/common';
 import { ApiModule } from './api/api.module';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
-import { AppController } from './app.controller';
+import { AppController } from './app.controller'
 import { TicketsModule } from './api/tickets/tickets.module';
 
 @Global()
