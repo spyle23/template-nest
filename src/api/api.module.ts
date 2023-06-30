1a1bdc865624ca714aa3900b819984a043daa725
spyle23 <sampingyen.lee@gmail.com>
2023-06-30 14:07:26 +0300

    reset

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -5,14 +5,11 @@ import {
   RequestMethod,
 } from '@nestjs/common';
 import { ApiController } from './api.controller';
-import { CommandModule } from './command/command.module';
-import { ProductModule } from './product/product.module';
-import { CategoryModule } from './category/category.module';
 import { AuthModule } from './auth/auth.module';
 import { LoggerMiddleware } from 'src/logger/logger.middleware';
 
 @Module({
-  imports: [CommandModule, ProductModule, CategoryModule, AuthModule],
+  imports: [AuthModule],
   controllers: [ApiController],
 })
 export class ApiModule implements NestModule {
--- src/api/command/command.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CommandController } from './command.controller';
-import { CommandService } from './command.service';
-
-@Module({
-  controllers: [CommandController],
-  providers: [CommandService],
-})
-export class CommandModule {}
