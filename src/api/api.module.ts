3d66427eb74bd7fda9cd1bfcb420d7f8f1ed4e0c
valdoth <valdotsiarohasina@gmail.com>
2023-06-30 15:27:20 +0300

    initialize tickets

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -7,9 +7,10 @@ import {
 import { ApiController } from './api.controller';
 import { AuthModule } from './auth/auth.module';
 import { LoggerMiddleware } from 'src/logger/logger.middleware';
+import { TicketsModule } from './tickets/tickets.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, TicketsModule],
   controllers: [ApiController],
 })
 export class ApiModule implements NestModule {
